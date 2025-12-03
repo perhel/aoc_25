a01703d53d49651a02feea6fda07101499fb5f6a
Per Hellström <perlarsgustav@gmail.com>
2025-12-03 09:21:35 +0100

    Day03

---
+++ App/03/Run.cs
@@ -0,0 +1,57 @@
+﻿
+namespace App._03
+{
+    internal class Run : IDay
+    {
+        public string Part1(IEnumerable<string> input)
+        {
+            int joltageNumberCount = 2;
+            IEnumerable<long> batteries = input.Select(l => l.Trim())
+                .Select(line => line.Select(c => c - '0').ToArray())
+                .Select(digitArray => BuildJoltage(ref digitArray, joltageNumberCount));
+
+            return batteries.Sum().ToString();
+        }
+
+        public string Part2(IEnumerable<string> input)
+        {
+            int joltageNumberCount = 12;
+            IEnumerable<long> batteries = input.Select(l => l.Trim())
+                .Select(line => line.Select(c => c - '0').ToArray())
+                .Select(digitArray => BuildJoltage(ref digitArray, joltageNumberCount));
+
+            return batteries.Sum().ToString();
+        }
+
+        private static long BuildJoltage(ref int[] digits, int joltageNumberCount)
+        {
+            long joltage = 0;
+            for (int i = joltageNumberCount - 1; i >= 0; i--)
+            {
+                int indexOfMax = IndexOfMaxDigitWithAtleastNTrailing(digits, i);
+                joltage = joltage * 10 + digits[indexOfMax];
+
+                Span<int> trail = ((Span<int>)digits)[(indexOfMax + 1)..];
+                digits = trail.ToArray();
+            }
+            return joltage;
+        }
+
+        private static int IndexOfMaxDigitWithAtleastNTrailing(int[] digits, int nTrail)
+        {
+            long max = -1;
+            int indexOfMax = -1;
+            for (int i = 0; i < digits.Length - nTrail; i++)
+            {
+                if (digits[i] > max)
+                {
+                    max = digits[i];
+                    indexOfMax = i;
+                }
+            }
+            return indexOfMax;
+        }
+
+        public void Reset() { }
+    }
+}
